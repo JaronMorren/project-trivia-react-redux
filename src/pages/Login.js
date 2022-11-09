7ea944516492e7f667453b4aea486a675c4ef9b6
Yanni <yannidilly.prog@gmail.com>
2022-11-09 16:38:46 -0300

    implementa página de login

---
+++ src/pages/Login.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailInput: '',
+      nameInput: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  buttonDisabledControl = () => {
+    const { emailInput, nameInput } = this.state;
+    if (nameInput.length !== 0 && emailInput.length !== 0) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    }
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    }, () => this.buttonDisabledControl());
+  };
+
+  render() {
+    const { emailInput, nameInput, isButtonDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email:
+          <input
+            id="email"
+            type="email"
+            name="emailInput"
+            value={ emailInput }
+            onChange={ this.handleChange }
+            data-testid="input-gravatar-email"
+            placeholder="Qual é o seu email do seu avatar?"
+          />
+        </label>
+        <label htmlFor="name">
+          Nome:
+          <input
+            id="name"
+            type="text"
+            name="nameInput"
+            value={ nameInput }
+            onChange={ this.handleChange }
+            data-testid="input-player-name"
+            placeholder="Qual é o seu nome?"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ isButtonDisabled }
+        >
+          Play
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Login;
